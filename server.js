abd60e98e71e4b35950fb61ef8e10ba829c2377e
shanik hernandez <shaniik.her93@gmail.com>
2020-04-09 21:03:41 -0400

    added html routes

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const path = require("path");
 
 const PORT = process.env.PORT || 8080; 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "public/notes.html"));
 
 app.listen(PORT, () => {
     console.log("Listening on port" + PORT)
