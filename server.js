5c78fa8d004557854b853d6404f5bd6c74607389
shanik hernandez <shaniik.her93@gmail.com>
2020-04-09 20:52:53 -0400

    port is listening

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require ("express");
-
 const app = express();
+const path = require("path");
 
-const PORT = 8080; 
+const PORT = process.env.PORT || 8080; 
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 
 app.listen(PORT, () => {
-    console.log("Listening on port 8080")
+    console.log("Listening on port" + PORT)
 })
+
