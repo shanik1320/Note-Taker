69738a74696b94c6d9fbfebf7b2c041809f94277
shanik hernandez <shaniik.her93@gmail.com>
2020-04-09 23:57:29 -0400

    trying to get the notes page to open

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require ("express");
 const app = express();
 const path = require("path");
 const fs = require ("fs");
-const db = require("..db/db.json")
-
 const PORT = process.env.PORT || 8080; 
 
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +20,7 @@ module.exports = function(app) {
       app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
       });
-
+      //api requests
       app.get("/api/notes", function (req, res) {
           res.json(db)
       });
@@ -33,11 +31,9 @@ module.exports = function(app) {
             text: req.body.text 
         };
     })
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-        if (err) throw err;
+    
 
-
-    })
+    });
 
     };
 
