2584df403343c604c6bac78eb023c20e8ee76952
shanik hernandez <shaniik.her93@gmail.com>
2020-04-10 21:30:46 -0400

    trying to link home page to note page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-// const db = require("../db/db")
+const stack = require("stack")
 
 const PORT = process.env.PORT || 8080;
 
@@ -14,22 +14,21 @@ app.use(express.static("public"));
 
 // HTML get requests
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-
-app.get("/api/tables", function (req, res) {
+app.get("/api/notes", function (req, res) {
     res.json(db);
 });
 
-app.post("/api/tables", function (req, res) {
+app.post("/api/notes", function (req, res) {
 
 
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(PORT, () => {
