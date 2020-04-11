7ec0bea9338a30e34ee34a9f818ea8161c1dd739
shanik hernandez <shaniik.her93@gmail.com>
2020-04-10 20:57:54 -0400

    deleted a function that wasnt needed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const db = require("../db/db")
+// const db = require("../db/db")
 
 const PORT = process.env.PORT || 8080;
 
